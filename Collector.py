68671f0708db74cd36e55f406d4d535152ddb005
tzattack <tzattack@gmail.com>
2016-07-28 16:41:36 +0800

    Initial commit

---
+++ Collector.py
@@ -0,0 +1,60 @@
+import os
+import Generator
+
+directories = [name for name in os.listdir('./') if os.path.isdir(os.path.join('./', name))]
+directories.remove(".idea")
+directories.remove("__pycache__")
+print(directories)
+
+client_ip = ''
+brand = ''
+platform = ''
+version = ''
+action = ''
+
+counter = 0
+for dir in directories:
+    subdirectory = './' + directories[counter] + '/result/'
+    print(subdirectory)
+    capture_names = [name for name in os.listdir(subdirectory) if os.path.isdir(os.path.join(subdirectory, name))]
+    print(capture_names)
+
+    capture_counter = 0
+    for cap in capture_names:
+        name = capture_names[capture_counter]
+        print(name)
+
+        name_counter = 0
+        name_divider = 0
+        for b in name:
+            if name[name_counter] == '_':
+                name_divider += 1
+
+            if name[name_counter] != '_':
+                if name_divider == 0:
+                    client_ip += name[name_counter]
+                elif name_divider ==1:
+                    brand += name[name_counter]
+                elif name_divider == 2:
+                    platform += name[name_counter]
+                elif name_divider == 3:
+                    version += name[name_counter]
+                else:
+                    action += name[name_counter]
+
+            name_counter += 1
+
+        print(client_ip)
+        print(brand)
+        print(platform)
+        print(version)
+        print(action)
+        client_ip = ''
+        brand = ''
+        platform = ''
+        version = ''
+        action = ''
+
+        capture_counter += 1
+
+    counter += 1
---
+++ Generator.py
@@ -0,0 +1,18 @@
+def generator(dir_name, site_type):
+    json_file = dir_name + site_type + ".json"
+    file = open(json_file, "w")
+
+    content = "{\"" + dir_name + "\":\n"
+    content += "\t{\n"
+    content += "\t\t\"bin\":{\"device:/com/roles/front/app/yjhp/yj_hp\"},\n"
+    content += "\t\t\"config\":[{\"device:/com/cfg/yj_hp_in.conf\"},{\"device:/com/cfg/yj_hp_output.conf\"},"
+    content += "{\"snspro:/com/cfg/yj_snp_extract.conf}\",{\"snspro:/com/cfg/yj_snp_output.conf}\"],\n"
+    content += "\t\"SupportVersion\":[{\"pc\":\"20160718\"}],"
+
+    file.write(content)
+    file.close()
+    return 0
+
+name = "21CN"
+site = "BBS"
+generator(name, site)
